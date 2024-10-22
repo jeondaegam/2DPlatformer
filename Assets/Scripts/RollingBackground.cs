bd5d0f1d8d824338ec646ad75bc3534cb71a5c3e
yeoreum <jeondaegam@naver.com>
2024-10-22 17:13:42 +0900

    feat: 배경 롤링 방향 변경 추가
    - 배경이미지가 흐르는 방향을 선택할 수 있도록 수정

--- Assets/Scripts/RollingBackground.cs
+++ Assets/Scripts/RollingBackground.cs
@@ -7,16 +7,29 @@ public class RollingBackground : MonoBehaviour
     public float speed = 5f;
     public float size;
 
+    public bool isRollingLeft = true;
     // 이 때 첫번째 이미지의 x position을 0으로 맞춰주는게 중요
     // 예를들어 첫 이미지의 x position이 1일 경우 1만큼 공백 뒤에 다음 이미지가 생성되므로 계산이 필요하다 ~ 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
-
-        if (transform.position.x < -size)
+        if (isRollingLeft)
         {
-            transform.Translate(new Vector2(size * 2, 0));
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+            if (transform.position.x < -size)
+            {
+                transform.Translate(new Vector2(size * 2, 0));
+            }
+
         }
+        else
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            if (transform.position.x > size)
+            {
+                transform.Translate(new Vector2(size * -2, 0));
+            }
+        }
+
     }
 }
